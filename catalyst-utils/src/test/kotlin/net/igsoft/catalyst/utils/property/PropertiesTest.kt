2a9b5cb9ddf5906a926505f689d92d36561d3991
Marcin <aarti@interia.pl>
2020-10-29 15:59:32 +0100

    Last file converted to Kotlin.

--- catalyst-utils/src/test/kotlin/net/igsoft/catalyst/utils/property/PropertiesTest.kt
+++ catalyst-utils/src/test/kotlin/net/igsoft/catalyst/utils/property/PropertiesTest.kt
@@ -1,69 +1,65 @@
-package net.igsoft.catalyst.utils.property;
+package net.igsoft.catalyst.utils.property
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import net.igsoft.catalyst.utils.property.Properties.Companion.builder
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
 @DisplayName("Properties Test")
-class PropertiesTest {
-
+internal class PropertiesTest {
     @Test
     @DisplayName("Test Equals Equivalence Relation")
-    void testEqualsEquivalenceRelation() {
+    fun testEqualsEquivalenceRelation() {
         // Given
-        Properties properties1 = Properties.builder().withProperty("name", "test").build();
-        Properties properties2 = Properties.builder().withProperty("name", "test").build();
-        Properties properties3 = Properties.builder().withProperty("name", "test").build();
+        val properties1 = builder().withProperty("name", "test").build()
+        val properties2 = builder().withProperty("name", "test").build()
+        val properties3 = builder().withProperty("name", "test").build()
 
         // When-Then
         // is reflexive
-        assertThat(properties1.equals(properties1)).isTrue();
+        Assertions.assertThat(properties1.equals(properties1)).isTrue
         // is symmetric
-        assertThat(properties1.equals(properties2)).isTrue();
-        assertThat(properties2.equals(properties1)).isTrue();
+        Assertions.assertThat(properties1.equals(properties2)).isTrue
+        Assertions.assertThat(properties2.equals(properties1)).isTrue
         // is transitive
-        assertThat(properties2.equals(properties3)).isTrue();
-        assertThat(properties1.equals(properties3)).isTrue();
+        Assertions.assertThat(properties2.equals(properties3)).isTrue
+        Assertions.assertThat(properties1.equals(properties3)).isTrue
         // null reference
-        assertThat(properties1.equals(null)).isFalse();
+        Assertions.assertThat(properties1.equals(null)).isFalse
         // wrong type
-        assertThat(properties1.equals(5)).isFalse();
+        Assertions.assertThat(properties1.equals(5)).isFalse
     }
 
     @Test
     @DisplayName("Should Fail To Equals For Bigger Properties")
-    void shouldFailToEqualsForBiggerProperties() {
+    fun shouldFailToEqualsForBiggerProperties() {
         // Given
-        Properties properties1 =
-                Properties.builder().withProperty("name", "test").withProperty("fail", "something").build();
-        Properties properties2 = Properties.builder().withProperty("name", "test").build();
+        val properties1 = builder().withProperty("name", "test").withProperty("fail", "something").build()
+        val properties2 = builder().withProperty("name", "test").build()
 
         // When-Then
-        assertThat(properties1.equals(properties2)).isFalse();
+        Assertions.assertThat(properties1.equals(properties2)).isFalse
     }
 
     @Test
     @DisplayName("Should Fail To Equals For Different Properties")
-    void shouldFailToEqualsForDifferentProperties() {
+    fun shouldFailToEqualsForDifferentProperties() {
         // Given
-        Properties properties1 = Properties.builder().withProperty("myName", "test").build();
-        Properties properties2 = Properties.builder().withProperty("name", "test").build();
+        val properties1 = builder().withProperty("myName", "test").build()
+        val properties2 = builder().withProperty("name", "test").build()
 
         // When-Then
-        assertThat(properties1.equals(properties2)).isFalse();
+        Assertions.assertThat(properties1.equals(properties2)).isFalse
     }
 
     @Test
     @DisplayName("Should Fail To Equals For Different Properties Value")
-    void shouldFailToEqualsForDifferentPropertiesValue() {
+    fun shouldFailToEqualsForDifferentPropertiesValue() {
         // Given
-        Properties properties1 =
-                Properties.builder().withProperty("name", "tet").withProperty("fail", "something").build();
-        Properties properties2 =
-                Properties.builder().withProperty("name", "test").withProperty("fail", "something").build();
+        val properties1 = builder().withProperty("name", "tet").withProperty("fail", "something").build()
+        val properties2 = builder().withProperty("name", "test").withProperty("fail", "something").build()
 
         // When-Then
-        assertThat(properties1.equals(properties2)).isFalse();
+        Assertions.assertThat(properties1.equals(properties2)).isFalse
     }
 }
